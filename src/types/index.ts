3d590b6f4d014e9fa2390fc4b1e35f32b481cf16
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-02-22 23:06:10 +0200

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
+/* ====== CUSTOMERS ====== */
+
 export interface Customer {
   id: string;
   name: string;
   email: string;
-} 
+}
 
 export interface NewCustomer {
   firstName: string;
@@ -13,3 +15,37 @@ export interface NewCustomer {
   rewards: number;
   avatar?: string;
 }
+
+
+/* ====== CATEGORIES ====== */
+
+export interface Category {
+  id: string;
+  name: string;
+}
+
+export interface NewCategory {
+  id: string;
+  name: string;
+  description: string;
+  picture?: string;
+}
+
+
+/* ====== PRODUCTS ====== */
+
+export interface Product {
+  id: string;
+  name: string;
+  categoryId: string;
+}
+
+export interface NewProduct {
+  id: string;
+  name: string;
+  categoryId: string;
+  categoryName?: string;
+  numInStock: number;
+  unitPrice: number;
+}
+
