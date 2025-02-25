be80f19dd836339aecd76541c6b0d3e707745f53
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-02-25 22:33:07 +0200

    create useOrderSearch

---
+++ src/hooks/useOrderSearch.ts
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { fetchAllOrders, fetchFilteredOrders } from '@/stores/orders/orderSlice';
+import { AppDispatch } from '@/stores/store';
+
+const REFERENCE_REGEX = /^[a-zA-Z0-9-]+$/;
+
+function useOrderSearch(initialSearch: { reference: string }) {
+  const dispatch = useDispatch<AppDispatch>();
+  const [search, setLocalSearch] = useState(initialSearch);
+
+  // Validation
+  const handleSearchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = evt.target;
+
+    if (value === '' || REFERENCE_REGEX.test(value)) {
+      setLocalSearch((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
+  // Start search
+  const handleSearch = () => {
+    dispatch(fetchFilteredOrders(search));
+  };
+
+  // Show all products
+  const handleResetSearch = () => {
+    dispatch(fetchAllOrders());
+    setLocalSearch({ reference: '' });
+  };
+
+  return { search, handleSearchChange, handleSearch, handleResetSearch };
+}
+
+export default useOrderSearch;
