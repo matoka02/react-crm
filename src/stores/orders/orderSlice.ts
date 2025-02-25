6784619e2f2217aa80c32d2ae3522cd6b9415a58
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-02-25 19:59:41 +0200

    update orderSlice

--- src/stores/orders/orderSlice.ts
+++ src/stores/orders/orderSlice.ts
@@ -5,35 +5,59 @@ import {
   ActionReducerMapBuilder,
 } from '@reduxjs/toolkit';
 
+import { Order } from '@/stores/types/modelTypes';
+
 import { HttpMethod } from '../types/httpTypes';
 
-interface Order {
-  id: string;
-  customerId: string;
-  totalAmount: number;
-  status: 'pending' | 'completed' | 'canceled';
-}
+import getCustomerName from './customerUtils';
+import getProductCount from './productUtils';
 
 interface OrderState {
   orders: Order[];
+  // order: NewOrder | null;
   isLoading: boolean;
   error?: string;
+  snackbarOpen: boolean;
+  snackbarMessage: string;
+  snackbarSeverity: 'success' | 'error' | 'warning' | 'info';
+  searchOpen: boolean;
+  search: {
+    reference: string;
+  };
 }
 
 const initialState: OrderState = {
   orders: [],
+  // order: null,
   isLoading: false,
+  snackbarOpen: false,
+  snackbarMessage: '',
+  snackbarSeverity: 'info',
+  searchOpen: false,
+  search: { reference: '' },
 };
 
-export const fetchOrders = createAsyncThunk<Order[], void, { rejectValue: string }>(
+export const fetchAllOrders = createAsyncThunk<Order[], void, { rejectValue: string }>(
   'order/fetchOrders',
-  async (_: any, { rejectWithValue }: any) => {
+  async (_: any, { getState, rejectWithValue }: any) => {
     try {
       const response = await fetch('/api/orders', { method: HttpMethod.GET });
 
       if (!response.ok) throw new Error('Error loading orders');
 
-      return await response.json();
+      const orders: Order[] = await response.json();
+
+      if (!orders) throw new Error('Invalid order data from API');
+
+      const data = orders.map((order) => ({
+        ...order,
+        customerName: getCustomerName(order.customerId, getState),
+        productsCount: getProductCount(order.products),
+      }));
+
+      return data;
+
+      return orders;
     } catch (error: any) {
       return rejectWithValue(error.message);
     }
@@ -43,20 +67,32 @@ export const fetchOrders = createAsyncThunk<Order[], void, { rejectValue: string
 const orderSlice = createSlice({
   name: 'order',
   initialState,
-  reducers: {},
+  reducers: {
+    // error & snackbar
+    clearError(state) {
+      return { ...state, error: undefined, snackbarOpen: false };
+    },
+    // searchbar
+    setSearchOpen(state: OrderState, action: PayloadAction<boolean>) {
+      return { ...state, searchOpen: action.payload };
+    },
+    setSearch(state: OrderState, action: PayloadAction<{ reference: string }>) {
+      return { ...state, search: action.payload };
+    },
+  },
   extraReducers: (builder: ActionReducerMapBuilder<OrderState>) => {
     builder
-      .addCase(fetchOrders.pending, (state: OrderState) => ({
+      .addCase(fetchAllOrders.pending, (state: OrderState) => ({
         ...state,
         isLoading: true,
         error: undefined,
       }))
-      .addCase(fetchOrders.fulfilled, (state: OrderState, action: PayloadAction<Order[]>) => ({
+      .addCase(fetchAllOrders.fulfilled, (state: OrderState, action: PayloadAction<Order[]>) => ({
         ...state,
         isLoading: false,
         orders: action.payload,
       }))
-      .addCase(fetchOrders.rejected, (state: OrderState, action: PayloadAction<any>) => ({
+      .addCase(fetchAllOrders.rejected, (state: OrderState, action: PayloadAction<any>) => ({
         ...state,
         isLoading: false,
         error: action.payload,
