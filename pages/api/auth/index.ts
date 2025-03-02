1fcfe62b4405fd1c2f9f35850dbad9fa7d680fca
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-02 19:26:50 +0200

    update API-routes

---
+++ pages/api/auth/index.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next/dist/types';
+
+import DB from '@/lib/demo-db';
+
+function handler(req: NextApiRequest, resp: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return resp.status(405).json({ message: `Method ${req.method} Not Allowed` });
+  }
+
+  try {
+    const { username, password } = req.body;
+
+    if (username === 'admin@test.com' && password === 'password') {
+      return resp.status(200).json(DB.token);
+    }
+  } catch (error) {
+    return resp.status(500).json({ message: 'Incorrect login or password', error });
+  }
+
+  return resp.status(403).json({ error: 'Invalid credentials' });
+}
+
+export default handler;
