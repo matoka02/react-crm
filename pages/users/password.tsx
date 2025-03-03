046373a19d3e3814de58aba52cd4002b0acb6ad8
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-03 21:32:58 +0200

    add ChangePasswordPage

---
+++ pages/users/password.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+export default function ChangePasswordPage(): React.ReactElement {
+  return (
+    <Box>
+      <Typography variant="h1" sx={{ paddingTop: 200, margin: 'auto', width: '50%' }}>
+        Change Password ...
+      </Typography>
+    </Box>
+  );
+}
