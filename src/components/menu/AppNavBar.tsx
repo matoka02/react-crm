79af61229ee07364fa624526c074ae56e4afb91b
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-03 21:33:50 +0200

    fix components

--- src/components/menu/AppNavBar.tsx
+++ src/components/menu/AppNavBar.tsx
@@ -4,26 +4,29 @@ import React from 'react';
 
 interface AppNavBarProps {
   handleDrawerToggle: () => void;
+  styles: {
+    appBar: React.CSSProperties;
+  };
 }
 
-const styles = {
+const getStyles = {
   container: { flexGrow: 1 },
   title: { flexGrow: 1 },
   toolbar: { minHeight: 0 },
 };
 
-function AppNavBar({ handleDrawerToggle }: AppNavBarProps): React.ReactElement {
+function AppNavBar({ styles, handleDrawerToggle }: AppNavBarProps): React.ReactElement {
   const handleClick = () => {
     window.open(process.env.NEXT_PUBLIC_GITHUB_URL, '_blank');
   };
 
   return (
-    <AppBar position="fixed">
-      <Toolbar sx={styles.toolbar}>
+    <AppBar position="fixed" sx={styles}>
+      <Toolbar sx={getStyles.toolbar}>
         <IconButton edge="end" onClick={handleDrawerToggle} color="inherit" aria-label="menu">
           <Menu />
         </IconButton>
-        <Typography variant="h5" sx={styles.title}>
+        <Typography variant="h5" sx={getStyles.title}>
           Demo
         </Typography>
         <Tooltip title="Github" aria-label="Github">
