618902275720a8cd3cfd3f058cac6b42016c40f2
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-09 16:38:36 +0200

    update ChangePasswordPage

--- pages/users/password.tsx
+++ pages/users/password.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 export default function ChangePasswordPage(): React.ReactElement {
   return (
-    <Box>
-      <Typography variant="h1" sx={{ paddingTop: 200, margin: 'auto', width: '50%' }}>
-        Change Password ...
-      </Typography>
+    <Box
+      sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Typography variant="h1">Change Password ...</Typography>
     </Box>
   );
 }
